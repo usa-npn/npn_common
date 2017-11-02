e23902e253f154b3bb8e335a7917275827db149d
Paul Adams <adamspe@gmail.com>
2017-11-02 08:33:40 -0500

    update selection data retrieval for getSiteVisitFrequency.json

--- visualizations/observation-frequency/observation-frequency-selection.ts
+++ visualizations/observation-frequency/observation-frequency-selection.ts
@@ -16,24 +16,11 @@ export class ObservationFrequencySelection extends StationAwareVisSelection {
 
     dataCnt:number = 0;
     getData():Promise<any> {
-        return new Promise((resolve,reject) => {
-            let url = this.serviceUtils.apiUrl('/npn_portal/networks/getSiteVisitFrequency.json'),
-                params = {
-                    year: this.year,
-                    network_id: this.networkIds[0]
-                };
-            this.serviceUtils.cachedGet(url,params)
-                .then(data => {
-                    // massage data collapsing maps into simple arrays
-                    data.stations = Object.keys(data.stations).map(key => data.stations[key]);
-                    data.stations.forEach(s => {
-                        let arr = [1,2,3,4,5,6,7,8,9,10,11,12].map(i => s.months[i]);
-                        s.months = arr;
-                    });
-                    resolve(data);
-                })
-                .catch(reject);
-
-        });
+        let url = this.serviceUtils.apiUrl('/npn_portal/networks/getSiteVisitFrequency.json'),
+            params = {
+                year: this.year,
+                network_id: this.networkIds[0]
+            };
+        return this.serviceUtils.cachedGet(url,params);
     }
 }
