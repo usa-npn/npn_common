fab0dbbc4772c406f8adf7b19833a10ec2006ed3
Paul Adams <adamspe@gmail.com>
2017-11-21 11:15:59 -0600

    update to map vis statistics to not display complete on gdd maps.

--- visualizations/clipped-wms-map/clipped-wms-map-statistics.component.ts
+++ visualizations/clipped-wms-map/clipped-wms-map-statistics.component.ts
@@ -1,17 +1,19 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+import { ClippedWmsMapSelection } from './clipped-wms-map-selection';
+
 @Component({
     selector: 'clipped-wms-map-statistics',
     template: `
-    <table>
+    <table *ngIf="statistics">
         <tbody>
             <tr><td>Date</td><td>{{statistics.date | date:'longDate'}}</td></tr>
             <tr><td>Count</td><td>{{statistics.count}}</td></tr>
             <tr><td>Mean</td><td>{{statistics.mean | number:'1.3-3'}}</td></tr>
             <tr><td>Std Dev</td><td>{{statistics.stddev | number:'1.3-3'}}</td></tr>
-            <tr><td>Min</td><td>{{statistics.min}}</td></tr>
-            <tr><td>Max</td><td>{{statistics.max}}</td></tr>
-            <tr><td>Complete</td><td>{{statistics.percentComplete | number:'1.0-2'}}%</td></tr>
+            <tr><td>Min</td><td>{{statistics.min  | number:'1.0-3'}}</td></tr>
+            <tr><td>Max</td><td>{{statistics.max  | number:'1.0-3'}}</td></tr>
+            <tr *ngIf="!gdd"><td>Complete</td><td>{{statistics.percentComplete | number:'1.0-2'}}%</td></tr>
         </tbody>
     </table>
     `,
@@ -31,5 +33,15 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ClippedWmsMapStatisticsComponent {
     @Input()
-    statistics: any;
+    selection:ClippedWmsMapSelection;
+
+    statistics:any;
+    gdd:boolean;
+
+    ngDoCheck() {
+        if(this.selection && this.selection.data && this.selection.data.statistics) {
+            this.statistics = this.selection.data.statistics;
+            this.gdd = this.selection && /^gdd/.test(this.selection.layer.layerName);
+        }
+    }
 }
