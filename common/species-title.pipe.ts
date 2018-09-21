bb126d42ba3c3e9d75582683d1532e59539594cf
npnlee85@gmail.com <npnlee85@gmail.com>
2018-09-21 12:46:37 -0700

    Changing the way species common names are displayed

--- common/species-title.pipe.ts
+++ common/species-title.pipe.ts
@@ -10,10 +10,6 @@ export class SpeciesTitlePipe implements PipeTransform {
         if(item) {
             format = format||environment.appConfig.tagSpeciesTitle;
             if(format === 'common-name') {
-                if(item.common_name) {
-                    let lower = item.common_name.toLowerCase();
-                    return lower.substring(0,1).toUpperCase()+lower.substring(1);
-                }
                 return item.common_name;
             } else if(format === 'scientific-name') {
                 return `${item.genus} ${item.species}`;
